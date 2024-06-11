aa080c8d65fc4cbbe271ddf004251da0c2b065f3
pmallol <paulamallol@gmail.com>
2024-06-11 20:09:20 -0300

    feat: Update index page to include subtle variant for dish type badges
    This commit modifies the index page to include the "subtle" variant for the dish type badges in the recipe section. By adding the "variant" prop with the value "subtle" to the UBadge component, the dish type labels are displayed with a more subtle styling. This change enhances the visual presentation of the dish types and improves the overall design of the page.
    
    Co-authored-by: [Author Name]

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <UContainer>
     <h1 class="mt-4 font-bold text-xl pt-12 mb-4">{{ recipe.title }}</h1>
-    <UBadge v-for="label in recipe.dishTypes" :label="label" class="mr-2" />
+    <UBadge
+      v-for="label in recipe.dishTypes"
+      :label="label"
+      class="mr-2"
+      variant="subtle"
+    />
 
-    <section class="mt-4">
+    <section class="mt-4 flex flex-col md:flex-row-reverse gap-4 w-full">
       <NuxtImg
         :src="recipe.image"
         :alt="recipe.title"
@@ -12,34 +17,45 @@
       <p v-html="recipe.summary" class="text-lg max-w-[40ch]"></p>
     </section>
 
-    <section class="mt-8">
-      <h2 class="font-bold text-lg mb-4">Ingredients</h2>
+    <div class="flex flex-col md:flex-row gap-2 md:gap-8">
+      <section class="mt-8">
+        <h2 class="font-bold text-lg mb-4">Ingredients</h2>
 
-      <UTable :rows="recipe.extendedIngredients" :columns="columns">
-        <template #name-data="{ row }">
-          <span class="flex flex-row gap-2 items-center">
-            <NuxtImg
-              provider="spoonacular"
-              :src="row.image"
-              :width="20"
-              :height="20"
-            />
-            {{ row.name }}
-          </span>
-        </template>
-      </UTable>
-    </section>
+        <UTable :rows="recipe.extendedIngredients" :columns="columns">
+          <template #name-data="{ row }">
+            <span class="flex flex-row gap-2 items-center">
+              <NuxtImg
+                provider="spoonacular"
+                :src="row.image"
+                :width="20"
+                :height="20"
+              />
+              {{ row.name }}
+            </span>
+          </template>
+        </UTable>
+      </section>
 
-    <section class="mt-8">
-      <h2 class="font-bold text-lg mb-4">Steps</h2>
-      <ol>
-        <li v-for="{number, step} in recipe.analyzedInstructions?.[0].steps" class="text-lg flex items-baseline gap-2">
-          <span class="flex-shrink-0 inline-flex items-center justify-center rounded-full bg-primary-50 dark:bg-primary-400 dark:bg-opacity-190 text-primary-500 dark:text-primary-400 ring-1 ring-inset ring-primary font-bold h-5 w-5">{{number}}</span>
-          {{step}}
-        </li>
-      </ol>
-      
-    </section>
+      <section class="mt-8">
+        <h2 class="font-bold text-lg mb-4">Steps</h2>
+        <ol>
+          <li
+            v-for="{ number, step } in recipe.analyzedInstructions?.[0].steps"
+            class="text-lg flex items-baseline gap-2"
+          >
+            <span
+              class="flex-shrink-0 inline-flex items-center justify-center rounded-full bg-primary-50 dark:bg-primary-400 dark:bg-opacity-190 text-primary-500 dark:text-primary-400 ring-1 ring-inset ring-primary font-bold h-5 w-5"
+              >{{ number }}</span
+            >
+            {{ step }}
+          </li>
+        </ol>
+      </section>
+    </div>
+
+    <UButton external :src="recipe.sourceUrl" class="mt-4">
+      View original recipe on {{ recipe.sourceName }}
+    </UButton>
   </UContainer>
 </template>
 
