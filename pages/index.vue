2ab2341c5565d8b8da0f8b78f8f5fc264888a42c
pmallol <paulamallol@gmail.com>
2024-06-11 20:03:15 -0300

    feat: Add steps section to index page for recipe instructions

--- pages/index.vue
+++ pages/index.vue
@@ -29,6 +29,17 @@
         </template>
       </UTable>
     </section>
+
+    <section class="mt-8">
+      <h2 class="font-bold text-lg mb-4">Steps</h2>
+      <ol>
+        <li v-for="{number, step} in recipe.analyzedInstructions?.[0].steps" class="text-lg flex items-baseline gap-2">
+          <span class="flex-shrink-0 inline-flex items-center justify-center rounded-full bg-primary-50 dark:bg-primary-400 dark:bg-opacity-190 text-primary-500 dark:text-primary-400 ring-1 ring-inset ring-primary font-bold h-5 w-5">{{number}}</span>
+          {{step}}
+        </li>
+      </ol>
+      
+    </section>
   </UContainer>
 </template>
 
